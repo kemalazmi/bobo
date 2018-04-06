c67e692f6c6ed60dba6a2b270b61cda91263dc7a
Kemal Azmi <kemal.azmi@coderscolony.com>
2018-04-07 00:11:06 +0700

    update

---
+++ layout/header.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Boboyuk</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+
+
+
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="../assets/js/bootstrap.min.js"></script>
+  <script src="../assets/js/main.js"></script>
+
+
+</head>
+
+<body>
+<div class="container">
+  <nav class="navbar navbar-default navbar-boboyuk">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+          aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="brand" href="index.php">
+          <img src="../assets/img/logo.png" class="img-responsive">
+        </a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#" style="padding-right:5px;">Bahasa</a>
+
+          </li>
+          <li>
+
+            <a href="#" style="padding-left:0;">| English</a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="icon-nav1"></i>
+              Promo
+            </a>
+          </li>
+          <li>
+            <a href="#" class="clearfix">
+              <i class="icon-nav2"></i>
+              Cek Pemesanan
+            </a>
+          </li>
+
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle clearfix" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <i class="icon-nav3"></i>
+              Akun Saya
+              <i class="glyphicon glyphicon-menu-down" aria-hidden="true"></i>
+            </a>
+            <ul id="login-dp" class="dropdown-menu">
+              <li>
+                <div class="row">
+                  <div class="col-md-12">
+                    Login via
+                    <div class="social-buttons">
+                      <a href="#" class="btn btn-fb">
+                        <i class="fa fa-facebook"></i> Facebook</a>
+                      <a href="#" class="btn btn-tw">
+                        <i class="fa fa-twitter"></i> Twitter</a>
+                    </div>
+                    or
+                    <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                      <div class="form-group">
+                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                      </div>
+                      <div class="form-group">
+                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                        <div class="help-block text-right">
+                          <a href="">Forget the password ?</a>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> keep me logged-in
+                        </label>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="bottom text-center">
+                    New here ?
+                    <a href="#">
+                      <b>Join Us</b>
+                    </a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
+
+  </div>
---
+++ pages/index.php
@@ -0,0 +1,24 @@
+<?php include '../layout/header.php' ?>
+
+
+<section class="mainbanner bgimage">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12"> 
+				<div class="text-banner text-center">
+					<h3>Pesan Hotel Online, Murah & Banyak Promo</h3>
+					
+					<h4>Tersedia lebih dari 1.150.000 hotel di dunia</h4>
+
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="searchbox">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
