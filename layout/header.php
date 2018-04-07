52df0661294dae19663e5a813ced1a671998383d
Kemal Azmi <kemal.azmi@coderscolony.com>
2018-04-07 13:09:34 +0700

    merge

--- layout/header.php
+++ layout/header.php
@@ -32,6 +32,7 @@
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="../assets/js/bootstrap.min.js"></script>
   <script src="../assets/js/jquery-ui.min.js"></script>
+  <script src="../assets/js/smooth-scroll.min.js"></script>
   <script src="../assets/js/main.js"></script>
 
 
---
+++ pages/hotel-page.php
@@ -0,0 +1,19 @@
+<?php include '../layout/header.php' ?>
+ 
+<section class="hotel-search">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-9">
+				<h5 class="judul">Jakarta Barat, Indonesia</h5>
+				<h5>23 Jan 2018 - 24 Jan 2018 ( 1 malam )  I  1 Kamar  I  2 Dewasa, 0 Anak</h5>
+			</div>
+			<div class="col-xs-3">
+				<div class="btn-pencarian">
+					<button type="button" class="btn btn-blue">button</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include '../layout/footer.php' ?> 
