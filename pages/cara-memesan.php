b2d411e3ce330e4962da4f32521207cb7d510ab8
Kemal Azmi <kemal.azmi@coderscolony.com>
2018-04-07 17:06:22 +0700

    update memesan

---
+++ pages/cara-memesan.php
@@ -0,0 +1,104 @@
+<?php include '../layout/header.php' ?>
+
+<section class="mainbanner bgimage">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="text-banner text-center">
+          <h3>Pesan Hotel Online, Murah & Banyak Promo</h3> 
+          <h4>Tersedia lebih dari 1.150.000 hotel di dunia</h4> 
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <?php include '../layout/searchbox.php' ?>
+      </div>
+    </div> 
+  </div>
+</section> 
+
+
+
+<section class="order">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				  <div class="heading-title">
+          <h2>
+        Cara Memesan
+
+
+          </h2>
+        </div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="list-h2o">
+					<div class="how-to-order">
+					<div class="img-order">
+						<img src="../assets/img/computer.png" alt="">
+					</div>
+					<div class="no-order">
+						<img src="../assets/img/1.png" alt="">
+					</div>
+					<div class="text-order">
+						Booking di lazada
+					</div>
+				</div>
+				<div class="how-to-order">
+					<div class="img-order">
+						<img src="../assets/img/nomorOrder.png" alt="">
+					</div>
+					<div class="no-order">
+						<img src="../assets/img/2.png" alt="">
+					</div>
+					<div class="text-order">
+						Setelah melakukan pembayaran di lazada,
+cantumkan nomer order dan email tanggal
+liburan ke <a href="">reservasi@boboyuk.com</a> 
+					</div>
+				</div>
+					<div class="how-to-order">
+					<div class="img-order">
+						<img src="../assets/img/emailPesan.png" alt="">
+					</div>
+					<div class="no-order">
+						<img src="../assets/img/3.png" alt="">
+					</div>
+					<div class="text-order">
+						Tunggu email konfirmasi dari tim reservasi
+<i>*email konfirmasi ketersediaan kamar akan 
+diberikan pada jam kerja dan hari kerja</i>
+					</div>
+				</div>
+				</div>
+				
+				
+			</div>
+		</div>
+			 
+	</div>
+</section>
+
+<?php include '../partials/download-app.php' ?>
+
+<section class="hr-spesial">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12"> 
+<hr class="spesial">
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include '../partials/tujuan-populer-lainnya.php' ?>
+
+
+
+
+<?php include '../layout/footer.php' ?>
